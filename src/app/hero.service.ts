49d44b79818a198034d18cfb86e6de20661eab40
Uwe Fuchs <info@uwefuchs.com>
2018-08-07 17:30:07 +0200

    consume heroes-service on heroku

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -14,7 +14,8 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class HeroService {
-  private heroesUrl = 'http://localhost:8080/heroes-service/webapi/heroes';  // URL to web api
+  //private heroesUrl = 'http://localhost:8080/heroes-service/webapi/heroes';  // URL to web api
+  private heroesUrl = 'https://heroes-service-heroku.herokuapp.com/heroes-service/webapi/heroes';  // URL to web api
 
   constructor(private http: HttpClient, private messageService: MessageService) {
   }
